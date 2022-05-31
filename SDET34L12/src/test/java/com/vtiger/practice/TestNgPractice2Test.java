5ada1643467440bfe8adc24d404ac81402c24b1a
omc <omc@DESKTOP-30D7VOM>
2022-05-31 12:07:31 +0530

    First commit

---
+++ SDET34L12/src/test/java/com/vtiger/practice/TestNgPractice2Test.java
@@ -0,0 +1,42 @@
+package com.vtiger.practice;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestNgPractice2Test extends TestNgBasicConfigAnnotationPracticeTest{
+
+	@Test(groups="sanity")
+	public void mohan1() {
+		Reporter.log("ashwin", true);
+	}
+	
+	@Test(groups="sanity")
+	public void mohan() {
+		Reporter.log("siddanth", true);
+}
+
+@Test(groups="sanity")
+public void practice1Test() {
+	Reporter.log("TestNgPractice2Test --> Test1", true);
+}
+@Test(groups="regression")
+public void practice2Test() {
+	Reporter.log("TestNgPractice2Test --> Test2", true);
+}
+@Test(groups="regression")
+public void practice3Test() {
+	Reporter.log("TestNgPractice2Test --> Test3", true);
+}
+
+@Test(groups="sanity")
+public void practice4Test() {
+	Reporter.log("TestNgPractice2Test --> Test4", true);
+}
+
+@Test(groups={"sanity","regression"})
+public void practice5Test() {
+	Reporter.log("TestNgPractice2Test --> Test5", true);
+}
+
+}
